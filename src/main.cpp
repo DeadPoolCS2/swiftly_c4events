03d3f00add727075bfd5ad19d27e2d2960357c39
Moongetsu <111883135+moongetsu@users.noreply.github.com>
2023-12-14 23:25:25 +0200

    Removed the client print 💩

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,6 @@ void TimerCallback() { // credits to blu (made some modifications)
 
     if (elapsedTime <= 10) {
         print("Debug pentru 10 secunde (%d) \n", elapsedTime);
-        g_playerManager->SendMsg(HUD_PRINTCONSOLE, "Debug pentru 10 secunde (%d) \n", elapsedTime);
         g_playerManager->SendMsg(HUD_PRINTCENTER, FetchTranslation("c4events.countdown.message"), elapsedTime);
     }
 
